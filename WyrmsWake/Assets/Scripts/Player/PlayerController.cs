b511edebe017d5d85998d8ecdacc1adce341ecdf
Abdulqadir Mohamedali <118472342+Abdulqadir53Mohamedali@users.noreply.github.com>
2025-08-19 11:17:55 +0100

    PlayerControlller scrpting started
    PlayerStats SO made

--- WyrmsWake/Assets/Scripts/Player/PlayerController.cs
+++ WyrmsWake/Assets/Scripts/Player/PlayerController.cs
@@ -1,7 +1,42 @@
+using JetBrains.Annotations;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Rendering.Universal.Internal;
+
 
 public class PlayerController : MonoBehaviour
 {
+    public PlayerStats baseStats;
+
+    public float Health;
+    public float Stamina;
+
+    Animator animator;
+    Rigidbody rb;
+
+    public bool isGrounded;
+
+    [SerializeField] private Transform cameraTransform;
+
+    public Vector2 movementInput;
+
+    public InputActionReference movementActionReference;
+    public InputActionReference JumpActionReference;
+    public InputActionReference CrouchActionReference;
+    public InputActionReference SprintActionReference;
+
+
+    public void Awake()
+    {
+        Health = baseStats.maxHealth;
+        Stamina = baseStats.maxStamina;
+
+        
+
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +46,18 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+    }
+    void FixedUpdate()
+    {
+        Vector3 forward = cameraTransform.forward;
+        Vector3 right = cameraTransform.right;
+
+        forward.Normalize();
+        right.Normalize();
+
+        Vector3 moveDirection = (forward * movementInput.x + right * movementInput.y).normalized;
+
         
     }
 }
---
+++ WyrmsWake/Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+[CreateAssetMenu(fileName = "PlayerStats", menuName = "Scriptable Objects/PlayerStats")]
+public class PlayerStats : ScriptableObject
+{
+    [Header("Movement")]
+    public float walkSpeed = 3.5f;
+    public float runSpeed = 5.5f;
+
+    [Header("Health")]
+    public float maxHealth = 100f;
+
+    [Header("Stamina")]
+    public float maxStamina = 100f;
+    public float staminaRegnPerSec = 8f;
+
+    [Header("Combat")]
+    public float lightDamage = 12f;
+    public float HeavyDamage = 22f;
+
+
+
+}
